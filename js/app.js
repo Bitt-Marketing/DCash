97cb8fab0f57f6dc5d0ab894067bd03224874cfb
Stephenio Abbott <stephenioabbott@gmail.com>
2020-09-10 14:50:48 -0400

    started consumer page - benefits v1

--- js/app.js
+++ js/app.js
@@ -1,24 +1,30 @@
-const text = new Blotter.Text('Digital.',{
-    family:'Arial Black',
-    size:64,
-    fill: '#3344b3',
-})
+let homeStartedBtns = document.querySelectorAll('.choose-path span');
+let homePaths = document.querySelectorAll('.path-information > div');
 
+for(let i = 0; i< homeStartedBtns.length; i++){
+    homeStartedBtns[i].addEventListener('click', function(){
+        homeStartedBtns.forEach(function(item, index){
+            item.classList.remove('active');
+            homePaths[index].classList.remove('active');
+        });
 
-const material = new Blotter.FliesMaterial();
-if(window.devicePixelRatio > 1){
-    material.uniforms.uPointCellWidth.value = 0.016;
-    material.uniforms.uPointRadius.value = 1.5;
-    material.uniforms.uSpeed.value = 2;
-}else{
-    material.uniforms.uPointCellWidth.value = 0.02089;
-    material.uniforms.uPointRadius.value = 10;  
-    material.uniforms.uSpeed.value = 2;
+        this.classList.add('active');
+        homePaths[i].classList.add('active');
+    })
 }
 
-const blotter = new Blotter(material, {
-    texts: text
-})
+/*
+var FRAMES = 148;
+var FPS = 30;
+var video = document.getElementById('video');
 
-const scope = blotter.forText(text);
-scope.appendTo(document.querySelector('i'));
+window.addEventListener('scroll', function (e) {
+  var time = (window.scrollY / 1000) * FRAMES / FPS;
+  video.currentTime = time;
+  console.log(time);
+});
+
+window.addEventListener('load', function(e) {
+  video.pause();
+  video.currentTime = 0;
+});*/
