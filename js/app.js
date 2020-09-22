2ad9977a0f7cbdf13e02339774419c38d0d6a826
Stephenio Abbott <stephenioabbott@gmail.com>
2020-09-22 10:55:37 -0400

    FI page v2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let contentDiv = document.getElementById('content');
+
 let homeStartedBtns = document.querySelectorAll('.choose-path span');
 let homePaths = document.querySelectorAll('.path-information > div');
 
@@ -16,52 +18,91 @@ for(let i = 0; i< homeStartedBtns.length; i++){
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".benefits",
-    start: "top top",
-    end: window.innerHeight * 2,
-    scrub: true,
-    pin: ".benefits .img",
-    anticipatePin: 1
-  }
-})
-.to(".benefits .img", {
-  scale: 1,
-  y: 100,
-  ease:  'none',
-});
+//Merchant Page
+if(contentDiv.classList.contains('consumer')){
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: ".benefits",
+      start: "top top",
+      end: window.innerHeight * 2,
+      scrub: true,
+      pin: ".benefits .img",
+      anticipatePin: 1
+    }
+  })
+  .to(".benefits .img", {
+    scale: 1,
+    y: 100,
+    ease:  'none',
+  });
+}
 
-let merchantFeat = document.querySelector('.landing .right').children;
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".feature-container",
-    start: "top 205px",
-    end: window.innerHeight * 2.5,
-    scrub: true,
-    pin: ".features",
-    anticipatePin: 1
-  }
-})
-.to(".intro-img", {
-  scale: 1,
-  x:0,
-  ease:  'none',
-})
-.to(merchantFeat, { opacity:1, scale: 1, stagger:0.15}, '-=0.45');
 
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".why-container",
-    start: "top 205px",
-    end: window.innerHeight * 4,
-    scrub: true,
-    pin: ".why",
-    anticipatePin: 1
+//Merchant Page
+if(contentDiv.classList.contains('business')){
+
+  let merchantFeat = document.querySelector('.landing .right').children;
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: ".feature-container",
+      start: "top 205px",
+      end: window.innerHeight * 2.5,
+      scrub: true,
+      pin: ".features",
+      anticipatePin: 1
+    }
+  })
+  .to(".intro-img", {
+    scale: 1,
+    x:0,
+    ease:  'none',
+  })
+  .to(merchantFeat, { opacity:1, scale: 1, stagger:0.15}, '-=0.45');
+
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: ".why-container",
+      start: "top 205px",
+      end: window.innerHeight * 4,
+      scrub: true,
+      pin: ".why",
+      anticipatePin: 1
+    }
+  })
+  .to(".why", {
+    scale: 1,
+    x:0,
+    ease:  'none',
+  }).to(".first .text", {duration: 0.1, opacity: 1}, "-=0.035");
+
+}
+
+//Fianancial Institutions PAge
+if(contentDiv.classList.contains('fi')){
+  const countryNav = document.querySelector('.country-nav ul');
+  const countryNavItems = countryNav.children;
+  const navPositions = [];
+  const navSizes = [];
+  const indicator = document.createElement('i');
+  indicator.className = 'indicator';
+
+  for(let i = 0; i < countryNavItems.length; i++){
+    navPositions[i] = countryNavItems[i].offsetLeft;
+    navSizes[i] = countryNavItems[i].offsetWidth;
   }
-})
-.to(".why", {
-  scale: 1,
-  x:0,
-  ease:  'none',
-}).to(".first .text", {duration: 0.1, opacity: 1}, "-=0.035");
+  console.log(navPositions);
+  countryNav.appendChild(indicator);
+  indicator.style.left = `${navPositions[0]}px`;
+  indicator.style.width = `${navSizes[0]}px`;
+
+  for(let i = 0; i < countryNavItems.length; i++){
+      countryNavItems[i].addEventListener('click', function(){
+        let activeNavitem = countryNav.querySelector('.active');
+        activeNavitem.classList.remove('active');
+        this.classList.add('active');
+        indicator.style.left = `${navPositions[i]}px`;
+        indicator.style.width = `${navSizes[i]}px`;
+
+      })
+  }
+}
